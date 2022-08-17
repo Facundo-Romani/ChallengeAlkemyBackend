9080bb918d7721cca9d3958123c0b9989560c00e
Facundo-Romani <facundoromani90@gmail.com>
2022-08-17 12:56:33 -0300

    User DB

---
+++ BackendDisneyApi/BackendDisneyApi/DataAccess/UserDBContext.cs
@@ -0,0 +1,27 @@
+﻿using BackendDisneyApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendDisneyApi.DataAccess
+{
+    public class UserDBContext : IdentityDbContext<User>
+    {
+        private const string Schema = "users";
+
+        public UserDBContext(DbContextOptions<UserDBContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema(Schema);
+
+            builder.Entity<User>()
+            .Property(x => x.IsActive)
+            .HasDefaultValue(true);
+        }
+    }
+}
+
